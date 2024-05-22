d6c8a6bb2a275836e01f0d32d7e30cb6d8b3cd1b
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-05-22 15:26:05 -0400

    Split out terminal print operations for use outside of error reporting

---
+++ src/common/terminal.rs
@@ -0,0 +1,45 @@
+use std::io::Write;
+use termcolor::{Buffer, Color, ColorSpec, WriteColor};
+
+// ===========================
+// = Terminal Printing Logic =
+// ===========================
+
+const WRITE_ERR: &str = "Uh oh, something went wrong while printing to terminal";
+
+pub fn color(s: String, buffer: &mut Buffer, bold: bool, italics: bool, c: Color) {
+    buffer.set_color(ColorSpec::new().set_bold(bold).set_italic(italics).set_fg(Some(c))).expect(WRITE_ERR);
+    write!(buffer, "{}", s.as_str()).expect(WRITE_ERR);
+}
+
+pub fn black(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Black)
+}
+pub fn blue(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Blue)
+}
+pub fn cyan(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Cyan)
+}
+pub fn green(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Green)
+}
+pub fn magenta(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Magenta)
+}
+pub fn magenta_italics(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, true, Color::Magenta)
+}
+pub fn red(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Red)
+}
+pub fn white(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Rgb(193,193,193))
+}
+pub fn yellow(bold: bool, s: String, buffer: &mut Buffer) {
+    color(s, buffer, bold, false, Color::Yellow)
+}
+pub fn long_line(buffer: &mut Buffer) {
+    let s = "--".repeat(50);
+    color(s, buffer, false, false, Color::White);
+}
