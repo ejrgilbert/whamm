48e9e5e19821916a45c1f1e3dfb197a02fee1ac1
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-07-20 11:39:05 -0400

    change .mm file to not depend on function names

--- tests/scripts/fault_injection/dfinity/dfinity_async_strcmp_fn.mm
+++ tests/scripts/fault_injection/dfinity/dfinity_async_strcmp_fn.mm
@@ -1,6 +1,7 @@
 /*
  * This will perform an asynchronous fault in Dfinity (redirect call to fault_injector canister endpoint).
  */
+i32 count;
 wasm::call:alt /
     target_fn_type == "import" &&
     target_imp_module == "ic0" &&
@@ -8,5 +9,7 @@ wasm::call:alt /
     strcmp((arg0, arg1), "bookings") &&
     strcmp((arg2, arg3), "record")
 / {
-    new_target_fn_name = "instr_redirect_to_fault_injector";
+// TODO -- change back to new_target_fn_name body when able to lookup function names with Orca
+//     new_target_fn_name = "instr_redirect_to_fault_injector";
+    count = 10;
 }
