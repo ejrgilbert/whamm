dda62ccaaff5ac6651da9be87dbe68dc0bc56e80
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-11-05 15:08:42 -0500

    Create skeleton to plan for 'alloc' var logic

---
+++ src/lang_features/alloc_vars/mod.rs
@@ -0,0 +1,2 @@
+pub mod rewriting;
+pub mod wizard;
---
+++ src/lang_features/alloc_vars/rewriting.rs
@@ -0,0 +1,14 @@
+use crate::parser::types::DataType;
+use crate::verifier::types::VarAddr;
+use std::collections::HashMap;
+
+pub fn allocate_vars(_to_alloc: Vec<(String, DataType)>) -> HashMap<String, (VarAddr, DataType)> {
+    // Called once per probe match point with `alloc` OR `report` vars.
+
+    // result: The new global variables: name -> (addr, ty)
+    //     as a hashmap to enable caller to place in SymbolTable and handle report variables
+    //   Add these VarAddrs to the symbol table.
+    //   Can now emit the rest of the probe body logic as normal.
+
+    todo!()
+}
