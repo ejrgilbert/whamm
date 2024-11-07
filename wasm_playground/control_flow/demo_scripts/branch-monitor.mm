836e5262720da9e403e75d7daeaf1cf7ba2748d8
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-11-07 17:32:31 -0500

    add in a demo skeleton

---
+++ wasm_playground/control_flow/demo_scripts/branch-monitor.mm
@@ -0,0 +1,10 @@
+// Branch Monitor Demo Agenda:
+// - basic implementation with global `map`
+// - add `report` variables
+// - add `unshared` variables
+
+// Profile the branches taken by `br`
+// TODO
+
+// Profile the branches taken by `br_if`
+// TODO
