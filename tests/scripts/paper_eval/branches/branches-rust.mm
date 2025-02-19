dd259c118371229329e4e08f3e44a9c7b96a2f5a
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2025-02-19 11:53:42 -0500

    Add in a branches test on rust

---
+++ tests/scripts/paper_eval/branches/branches-rust.mm
@@ -0,0 +1,22 @@
+wasm::br_if:before {
+  report unshared var taken: i32;
+  report unshared var not_taken: i32;
+
+  // which branch was taken?
+  if (arg0 != 0) {
+    taken++;
+  } else {
+    not_taken++;
+  }
+}
+
+wasm::br_table:before {
+  report unshared var taken_branches: map<u32, u32>;
+
+  // which branch was taken?
+  // default branch is at 'num_targets' in the map
+  var index: u32 = arg0 <= (num_targets - 1) ? arg0 : num_targets;
+
+  // count stores an array of counters
+  taken_branches[index]++;
+}
