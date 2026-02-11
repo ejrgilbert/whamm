c1271026f29060f788890a3e142984257e59216a
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2026-02-11 10:06:05 -0500

    save my old version of the test

--- tests/scripts/core_suite/strings/get-from-lib-global.mm
+++ tests/scripts/core_suite/strings/get-from-lib-global.mm
@@ -7,11 +7,11 @@ var ptr: i32 = alpha.mem_alloc(MAX);
 
 // write a string to memory, return the length of the string written
 // (should also get flushed at the end of execution)
-var l: i32 = alpha.write_alphabet(ptr, MAX);
+report var l: i32 = alpha.write_alphabet(ptr, MAX);
 
 // read the string from the library's memory
-// TODO: make `report var string` work!
-var s: str = read_str(memid(alpha), ptr, l as u32);
+// (should also get flushed at the end of execution)
+report var s: str = read_str(memid(alpha), ptr, l as u32);
 
 var core_ptr: i32 = whamm_core.mem_alloc(l as i32);
 write_str(memid(whamm_core), core_ptr, s);
@@ -20,9 +20,8 @@ whamm_core.mem_free(core_ptr);
 
 wasm:opcode:drop:before {
     // read the string from the library's memory that should still be live
-    // TODO: make `report var string` work!
-    var s1: str = read_str(memid(alpha), ptr, l as u32);
-
+    // (should also get flushed at the end of execution)
+    report var s1: str = read_str(memid(alpha), ptr, l as u32);
 
     core_ptr = whamm_core.mem_alloc(l as i32);
     write_str(memid(whamm_core), core_ptr, s1);
